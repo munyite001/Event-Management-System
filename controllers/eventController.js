5e40d0032e65926cee1b2435095cbe99f99f4f57
munyite001 <emunyite@gmail.com>
2024-05-26 17:40:01 +0300

    finished crud operations for events

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -141,21 +141,123 @@ exports.event_create_post = [
 
 // Display event delete form on GET.
 exports.event_delete_get = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented: event delete get")
+   const event = await Event.findById(req.params.id).exec()
+    if (event == null) {
+         res.redirect("/catalog/events")
+    }
+    res.render("event_delete", {
+        title: "Events",
+        event: event
+    })
 })
 
 // Handle event delete on POST.
 exports.event_delete_post = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented: event delete post")
+    const event = await Event.findById(req.body.eventid).exec()
+
+    if (event == null) {
+        res.redirect("/catalog/events")
+    }
+
+    await Event.findByIdAndDelete(req.body.eventid).exec()
+    res.redirect("/catalog/events")
 })
 
 // Display event update form on GET.
 exports.event_update_get = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented: event update get")
+   const [event, allVenues, allOrganizers, allParticipants] = await Promise.all([
+         Event.findById(req.params.id).populate("venue").populate("organizer").populate("participants").exec(),
+         Venue.find({}).sort({name: 1}).exec(),
+         Organizer.find({}).sort({name: 1}).exec(),
+         Participant.find({}).sort({name: 1}).exec()
+   ])
+
+   if (event == null) {
+    const err = new Error("Event not found");
+    err.status = 404;
+    return next(err);
+   }
+
+   allParticipants.forEach((participant) => {
+    if (event.participants.includes(participant._id)) {
+        participant.isSelected = "true";
+    }
+   })
+
+   res.render("event_form", {
+    title: "Events",
+    subTitle: "Update Event",
+    event: event,
+    venues: allVenues,
+    organizers: allOrganizers,
+    participants: allParticipants
+   })
 })
 
 // Handle event update on POST.
-exports.event_update_post = asyncHandler(async (req, res, next) => {
-    res.send("Not implemented: event update post")
-})
+exports.event_update_post = [
+    body("title")
+    .trim()
+    .isLength({min: 3})
+    .withMessage("Title should be atleast 3 letters")
+    .escape(),
+    body("description")
+    .trim()
+    .isLength({min: 3})
+    .withMessage("Description should be at least 3 letters long")
+    .escape(),
+    body("date", "Invalid date").optional({checkFalsy: true}).isISO8601().toDate(),
+    body("time", "Invalid time"),
+    body("venue", "Venue must not be empty").trim().isLength({min: 1}).escape(),
+    body("organizer", "Organizer must not be empty").trim().isLength({min: 1}).escape(),
+    body("participants", "Participants must not be empty").trim().isLength({min: 1}).escape(),
 
+    // Process request after validation and sanitization
+    asyncHandler(async (req, res, next) => {
+        // Extract the validation errors from a request
+        const errors = validationResult(req);
+
+        // Create a Event object with escaped and trimmed data
+        const event = new Event({
+            title: req.body.title,
+            description: req.body.description,
+            date: req.body.date,
+            time: req.body.time,
+            venue: req.body.venue,
+            organizer: req.body.organizer,
+            participants: req.body.participants,
+            _id: req.params.id
+        })
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values/error messages
+            const [allVenues, allOrganizers, allParticipants] = await Promise.all([
+                Venue.find({}).sort({title: 1}).exec(),
+                Organizer.find({}).sort({name: 1}).exec(),
+                Participant.find({}).sort({name: 1}).exec()
+            ])
+
+        for (const participant of participants) {
+            if (req.body.participants.includes(participant._id)) {
+                participant.isSelected = "true";
+            }
+        }
+
+            res.render("event_form", {
+                title: "Events",
+                subTitle: "Create New Event",
+                venues: allVenues,
+                organizers: allOrganizers,
+                participants: allParticipants,
+                event: event,
+                errors: errors.array()
+            })
+            return
+        } else {
+            // Data from form is valid. Save event
+            const updatedEvent = await Event.findByIdAndUpdate(req.params.id, event, {})
+            res.redirect(updatedEvent.url)
+        }
+    })
+
+]
